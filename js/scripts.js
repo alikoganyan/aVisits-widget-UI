1c9945739e2fa530cc0941ef18469c398964e584
alik oganyan <alik.oganyan@yandex.ru>
2017-11-21 18:23:55 +0300

    6 page

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+	// $('select').styler({
+	// 	selectSearch: true
+	// });
+	
+	// $('#scrollbox').enscroll({
+	// 	verticalTrackClass: 'track1',
+	// 	verticalHandleClass: 'handle1',
+	// 	drawScrollButtons: false,
+	// });
+	
+	if($('#accordion').length){
+		
+		$('#accordion > li > a').click(function(){
+			if($(this).closest('li').hasClass('avisits-active')) {
+				$(this).closest('li').removeClass('avisits-active');
+			} else {
+				$('#accordion > li').removeClass('avisits-active');
+				$(this).closest('li').addClass('avisits-active');
+			}
+			return false;
+		});
+	}
+	
+	$(".avisits-date_slider").slick({
+        infinite: true,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+		dots: false
+    });
+	
+	$(".avisits_slider").slick({
+        infinite: true,
+		centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		dots: false,
+		arrows: false
+    });
+	
+	$('ul.tabs').delegate('li:not(.current)', 'click', function() {
+		$(this).addClass('current').siblings().removeClass('current')
+			.parents('').find('div.box').addClass('active').eq($(this).index()).removeClass('active');
+	})
+		
+});
+
+
+$(function(){
+	$('input[placeholder], textarea[placeholder]').placeholder();
+});
